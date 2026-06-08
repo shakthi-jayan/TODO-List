9b44bc624412326232e3da8cd88e8cff812593aa
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-06-08 07:18:53 +0530

    Update AddTask.jsx

--- frontend/pages/AddTask.jsx
+++ frontend/pages/AddTask.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-const API = "https://todo-list-vflu.onrender.com"
+const API = "https://todo-list-8r3s.vercel.app"
 
 const AddTask = () => {
   const [title, setTitle] = useState("")
